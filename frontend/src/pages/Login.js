155dc1f59b9d43a3676b2d46865c9445e94ea674
himanshi-gupta <himanshigupta579@gmail.com>
2022-11-06 03:33:04 +0530

    added logi signup pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import "../styles/LoginSignup.css"
 
-function login() {
+const Login = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      login
+    <div className="wrapper fadeInDown">
+      <div id="formContent">
+        <h2 className="active"> Sign In </h2>
+        <h2 className="inactive underlineHover" onClick={() => navigate("/register")}>Sign Up </h2>
+
+        <div className="fadeIn first">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" id="icon" alt="User Icon" />
+        </div>
+
+        <form>
+          <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" onChange={onChange} />
+          <input type="text" id="password" className="fadeIn third" name="password" placeholder="password" onChange={onChange} />
+          <input type="submit" className="fadeIn fourth" value="Log In" onClick={onSubmit} />
+        </form>
+
+        <div id="formFooter">
+          <a className="underlineHover" href="#">Forgot Password?</a>
+        </div>
+
+      </div>
     </div>
   )
 }
 
-export default login
+export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
 
-function register() {
+const Register = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { password, password2 } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    }
+  }
+
   return (
-    <div>
-      register
+    <div className="wrapper fadeInDown">
+      <div id="formContent">
+        <h2 className="inactive underlineHover" onClick={() => { navigate('/login') }}> Sign In </h2>
+        <h2 className="active">Sign Up </h2>
+
+        <div className="fadeIn first">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" id="icon" alt="User Icon" />
+        </div>
+
+        <form>
+          <input type="text" id="name" className="fadeIn second" name="name" placeholder="name" onChange={onChange} />
+          <input type="text" id="username" className="fadeIn second" name="username" placeholder="username" onChange={onChange} />
+          <input type="text" id="email" className="fadeIn second" name="email" placeholder="email" onChange={onChange} />
+          <input type="text" id="password" className="fadeIn third" name="password" placeholder="password" onChange={onChange} />
+          <input type="text" id="password2" className="fadeIn third" name="password2" placeholder="confirm password" onChange={onChange} />
+          <input type="submit" className="fadeIn fourth" value="Log In" onClick={onSubmit} />
+        </form>
+
+        <div id="formFooter">
+          <a className="underlineHover" href="#">Forgot Password?</a>
+        </div>
+
+      </div>
     </div>
   )
 }
 
-export default register
+export default Register
